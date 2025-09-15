149752d8cf25a6240c608c02e59a62256ca51e15
Pratham Mishra <prathammishra845@gmail.com>
2025-09-15 17:24:52 +0530

    Update Sitecontroller.java

--- src/main/java/com/luv2code/emailVerification/demo/controller/Sitecontroller.java
+++ src/main/java/com/luv2code/emailVerification/demo/controller/Sitecontroller.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
-@RequestMapping("/start")
 public class Sitecontroller {
     @GetMapping("/")
     public String homePage(){
